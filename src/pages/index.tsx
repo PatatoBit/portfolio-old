220b42c059e09057dc9e3fcbb47accffd2d97f6a
PathonScript <pathonscript@gmail.com>
2022-09-03 23:05:32 +0700

    style: fonts and patato.live header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,10 +15,15 @@ const Home: NextPage = () => {
 
       <div className="fullflex bg-yellow-200">
         <div className="w-3/5 h-3/4 flex flex-col shadow-lg bg-white border-2 border-blue-900 rounded-lg">
-          <div className="w-full flex justify-end top">
-            <div className="border-blue-500 window-btn">➖</div>
-            <div className="border-green-500 window-btn">⏹️</div>
-            <div className="border-red-500 window-btn">❌</div>
+          <div className="w-full flex top">
+            <div className="flex flex-1 p-2">
+              <h1 className="text-4xl poppins">patato.live <span className="text-md text-grey">v.0.3</span></h1>
+            </div>
+            <div className="flex flex-1 justify-end">
+              <div className="border-blue-500 window-btn">➖</div>
+              <div className="border-green-500 window-btn">⏹️</div>
+              <div className="border-red-500 window-btn">❌</div>
+            </div>
           </div>
           <div className="flex flex-row bottom">
             <div className="section basis-1/5">
@@ -26,7 +31,6 @@ const Home: NextPage = () => {
                 <h1 className="nav">About me</h1>
                 <h1 className="nav">Portfolio</h1>
                 <h1 className="nav">Contacts</h1>
-                <h1 className="items-end">Version 1.0.0</h1>          
             </div>
             <div className="section basis-4/5">
 
